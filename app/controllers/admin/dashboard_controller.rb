c2dc26eda652ae652fc2fa68276e6579a2653d79
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-16 10:27:18 -0800

    add instance variable for dashboard controller so product and catagories can be displayed on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["ADMIN_username"], password: ENV["ADMIN_password"]
+
   def show
+    @categories_count = Category.count
+    @product_count = Product.count
   end
 end
